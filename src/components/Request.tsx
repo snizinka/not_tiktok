6b2b0b494f2199c4060b5c771b35f821912654e9
bohdan <bogdanosckinss@gmail.com>
2023-01-12 15:07:32 +0200

    Decorated forms and created slider

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -1,47 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import Header from './Header'
 
 export const Request = (props: any) => {
+    const [stage, setStage] = useState(0);
 
     return (
         <RequestDiv>
+            <Header></Header>
             <div className="form-wrapper">
-                <div className='request-container'>
-                    <div className="form-component">
-                        <div className='topic-box'>
-                            <p>Topic</p>
-                            <div className='topic-list'>
-                                <div className="topic-item">
-                                    <button className='remove-item'>x</button>
-                                    <a href='#1'>Science</a>
+                <div className="slider"
+                    style={{ transform: `translate(-${stage}%, 0px)` }}>
+                    <div className="first-stage">
+                        <div className='request-container'>
+                            <div className="form-component">
+                                <p>Topic</p>
+                                <input type="text" />
+                                <button>Add</button>
+                                <div className='topic-box'>
+                                    <div className='topic-list'>
+                                        <div className="topic-item">
+                                            <button className='remove-item'>x</button>
+                                            <a href='#1'>Science</a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+
+                            <div className='form-component'>
+                                <p>Task</p>
+                                <input type="text" />
+                            </div>
+
+                            <div className="form-component">
+                                <p>Budget</p>
+                                <input type="text" />
+                            </div>
+
+                            <div className="form-component">
+                                <p>Deadline</p>
+                                <input type="date" />
+                            </div>
+
+                            <div className="form-component">
+                                <p>More details (Word file)</p>
+                                <input type="file" />
+                            </div>
+
+                            <div className="continue">
+                                <button onClick={() => {
+                                    if (true) {
+                                        setStage(stage + 100)
+                                    }
+                                }}>➜</button>
+                            </div>
                         </div>
-                        <input type="text" />
                     </div>
 
-                    <div className='form-component'>
-                        <p>Task</p>
-                        <input type="text" />
-                    </div>
+                    <div className="second-stage">
+                        <div className='request-container'>
+                            <div className="form-component">
+                                <p>User to accomplish</p>
+                                <input type="text" className='search-user' />
+                                <button>Add</button>
+                            </div>
 
-                    <div className="form-component">
-                        <p>Budget</p>
-                        <input type="text" />
-                    </div>
+                            <div className="user-list">
+                                <div className="user-offered">
+                                    <p>Some user to accomplish</p>
+                                    <a href="#2">Visit profile</a>
+                                </div>
+                            </div>
 
-                    <div className="form-component">
-                        <p>Deadline</p>
-                        <input type="text" />
-                    </div>
+                            <div className="user-que">
 
-                    <div className="form-component">
-                        <p>More details (Word file)</p>
-                        <input type="file" />
-                    </div>
+                            </div>
 
-                    <div className="form-component">
-                        <p>User to accomplish</p>
+                            <div className="continue">
+                                <button onClick={() => {
+                                    if (true) {
+                                        setStage(stage + 100)
+                                    }
+                                }}>➜</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -52,20 +94,41 @@ export const Request = (props: any) => {
 
 const RequestDiv = styled.div`
 .form-wrapper {
-margin: auto;
+    margin: auto;
     padding: 20px 0;
     width: 1400px;
     background: #BDBDBD;
     box-shadow: 20px 20px 30px rgb(0 0 0 / 25%);
     border-radius: 10px;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    overflow: hidden;
+}
+
+.slider {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    transition: .7s ease;
+}
+
+.first-stage, .second-stage {
+    width: 100%;
+    display: flex;
     justify-content: center;
+    align-items: center;
 }
 
 .request-container {
     display: flex;
     flex-direction: column;
+    width: 500px;
+    background: #d9d9d9;
+    justify-content: center;
+    align-items: center;
+    border-radius: 30px;
+    gap: 20px;
+    padding: 30px 0;
 }
 
 .form-component {
@@ -76,30 +139,38 @@ margin: auto;
         font-size: 19px;
     }
 
-    input[type=text] {
+    input[type=text], input[type="date"] {
         border: none;
         outline: none;
         padding: 8px 10px;
-        width: 270px;
+        width: 330px;
         border-radius: 7px;
+        margin-bottom: 10px;
     }
 
+    width: 350px;
     display: flex;
     flex-direction: column;
 }
 
 .topic-box {
-    display: flex;
+    //display: flex;
 
     .topic-list {
         display: flex;
+        display: flex;
+        max-width: 400px;
+        flex-wrap: wrap;
+        gap: 6px 10px;
+        max-height: 60px;
+        overflow: auto;
     }
 
     .topic-item {
         display: flex;
-        padding: 2px 6px;
+        padding: 4px 8px;
         background: #FFF9D7;
-        border-radius: 7px;
+        border-radius: 10px;
 
         a {
             font-family: 'Signika Negative', sans-serif;
@@ -113,7 +184,17 @@ margin: auto;
             width: 14px;
             border: none;
             background: transparent;
+            font-family: 'Signika Negative',sans-serif;
+            font-weight: 600;
+            cursor: pointer;
         }
     }
+}
+
+.continue button {
+    height: 40px;
+    width: 40px;
+    border-radius: 30%;
+    cursor: pointer;
 }
 `
